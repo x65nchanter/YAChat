c958a0dc5e3fd74e018184260b9af0cdba207f5f
lisyamorda <skoopq@gmail.com>
2016-12-10 23:55:40 +0300

    Start new projeckt

---
+++ simpleChat/client/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTML_cycles
+{
+    class Program
+    {
+        static IPHostEntry ipHost = Dns.GetHostEntry("94.233.95.150");
+        static IPAddress ipAddress = ipHost.AddressList[0];
+        static IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, 80);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите логин");
+            string login = Console.ReadLine();
+            while (true)
+            {
+                Console.WriteLine("Введите сообщение: ");
+                string message = " "+login+ ": " + Console.ReadLine()+"!EOF";
+
+                byte[] data = Encoding.UTF8.GetBytes(message);
+                int i;
+                int x;
+                byte[] response = requestHTTP(data, out x, out i);
+                Console.Clear();
+                Console.WriteLine("Сообщение от сервера: ");
+                Console.WriteLine(Encoding.UTF8.GetString(response));
+                if (message.IndexOf(":q") > -1)
+                {
+                    break;
+                }
+            }
+
+        }
+
+        public static byte[] requestHTTP(byte[] message, out int byteSend, out int byteResponse)
+        {
+            byte[] response = new byte[1024];
+            Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            sender.Connect(ipEndPoint);
+            byteSend = sender.Send(message);
+            byteResponse = sender.Receive(response);
+            return response;
+        }
+    }
+    enum StandarstsPorts
+    {
+        HTTP = 80,
+        HTTPS = 8080,
+    }
+
+}
---
+++ simpleChat/server/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server
+{
+    class Program
+    {
+        static IPHostEntry ipHost = Dns.GetHostEntry("94.233.95.150");
+        static IPAddress ipAddress = ipHost.AddressList[0];
+        static IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, 80);
+
+        static void Main(string[] args)
+        {
+            getClient(7);
+        }
+
+        public static void getClient(int backlog)
+        {
+            Socket sListener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            Byte[] data = new byte[4096];
+            String message = null;
+            sListener.Bind(ipEndPoint);
+            sListener.Listen(backlog);
+
+            while (true)
+            {
+                Console.WriteLine("Ожидаю подключения на порте {0}", ipEndPoint);
+
+                Socket handler = sListener.Accept();
+
+                Byte[] tData = new byte[1024];
+
+                int byteResponse = handler.Receive(tData);
+
+                string tmessage = Encoding.UTF8.GetString(tData);
+
+                message += tmessage.Substring(0,tmessage.IndexOf("!EOF"))+"\n";
+                if (Encoding.UTF8.GetBytes(message).Length>1024)
+                {
+                    message = message.Substring(message.Length - 512);
+                }
+                Console.WriteLine(message);
+                byte[] response = new byte[1024];
+
+                response = Encoding.UTF8.GetBytes(message);
+                handler.Send(response);
+                if (tmessage.IndexOf(":sd") > -1)
+                {
+                    break;
+                }
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+            }
+
+        }
+    }
+}
